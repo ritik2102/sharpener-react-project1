eec65f6e7de7aa9000efb9870e4b7890bf4b39c3
ritik2102 <ritik21feb@gmail.com>
2024-01-18 16:16:32 +0530

    Initial commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <h2>March 20 2021</h2>
+            <div>
+                Date
+            </div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$20</div>
+            </div>
+        </div>
+
+
+    );
+}
+
+export default ExpenseItem;
