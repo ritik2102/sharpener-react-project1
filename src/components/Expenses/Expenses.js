2599da9e33517757b4f356e49c696c452deb8820
ritik2102 <ritik21feb@gmail.com>
2024-01-29 11:17:54 +0530

    Filtering the components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,17 +6,23 @@ import ExpenseFilter from "./ExpenseFilter";
 
 function Expenses(props) {
     
+    const [filteredComponents,setFilteredComponents]=useState(props.items);
     const[filteredYear,setFilteredyear] =useState('2020');
 
     const filterChangeHandler= selectedYear=>{
-        setFilteredyear(selectedYear);
+        setFilteredyear(selectedYear)
+    
+        const filtered=props.items.filter(component=>{
+            return Number(component.date.getFullYear())===Number(selectedYear);
+        });
+        setFilteredComponents(filtered);
     }
 
     return (
         <Card className='expenses'>
             <h2>Let's get started!</h2>
             <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-            {props.items.map((item,index) => {
+            {filteredComponents.map((item,index) => {
                 return <ExpenseItem 
                 id={item.id}
                 title={item.title}
@@ -25,40 +31,6 @@ function Expenses(props) {
                 LocationOfExpenditure={item.LocationOfExpenditure}
                 />
             })}
-            {/* <ExpenseItem 
-                id={expenses[0].id}
-                title={expenses[0].title}
-                date={expenses[0].date}
-                amount={expenses[0].amount}
-                LocationOfExpenditure={expenses[0].LocationOfExpenditure}
-                onDeleteExpense={handleDeleteExpense}
-            />
-
-            <ExpenseItem 
-                id={expenses[1].id}
-                title={expenses[1].title}
-                date={expenses[1].date}
-                amount={expenses[1].amount}
-                LocationOfExpenditure={expenses[1].LocationOfExpenditure}
-                onDeleteExpense={handleDeleteExpense}
-            />
-
-            <ExpenseItem 
-                id={expenses[2].id}
-                title={expenses[2].title}
-                date={expenses[2].date}
-                amount={expenses[2].amount}
-                LocationOfExpenditure={expenses[2].LocationOfExpenditure}
-                onDeleteExpense={handleDeleteExpense}
-            />
-            <ExpenseItem 
-                id={expenses[3].id}
-                title={expenses[3].title}
-                date={expenses[3].date}
-                amount={expenses[3].amount}
-                LocationOfExpenditure={expenses[3].LocationOfExpenditure}
-                onDeleteExpense={handleDeleteExpense}
-            /> */}
 
         </Card>
     );
