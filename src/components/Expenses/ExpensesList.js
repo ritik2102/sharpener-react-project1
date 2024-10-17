82029cf5f18d597d78cbd438d45252540f20daa9
ritik2102 <ritik21feb@gmail.com>
2024-10-17 22:24:42 +0530

    Conditional rendering

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -6,7 +6,7 @@ import './ExpensesList.css';
 const ExpensesList= props =>{
 
     if(props.items.length === 0){
-        return <h2 className="expenses-list__fallback">Found no expenses.</h2>
+        return <h2 className="expenses-list__fallback">No expenses found.</h2>
     }
     return <ul className="expenses-list">
         {props.items.map((item) => {
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
-const NewExpense =(props) =>{
+const NewExpense = (props) => {
 
+    
+    
+    const[viewForm,setViewForm]=useState(false);
+
+    const viewFormHandler=()=>{
+        setViewForm(true);
+    }
     // We are passing this function to the child component
-    const saveExpenseDataHandler=(enteredExpenseData)=>{
-        const expenseData={
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
             ...enteredExpenseData,
-            id:Math.random().toString()
+            id: Math.random().toString()
         };
+        setViewForm(false);
         props.onAddExpense(expenseData);
     };
+    if(viewForm === false){
+        return (
+            <div className='new-expense'>
+                <button onClick={viewFormHandler}> Add New Expense</button>
+            </div>
+        );
+    }
     return (
-        <div className='new-expense'> 
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+        <div className='new-expense'>
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
         </div>
     );
 };
